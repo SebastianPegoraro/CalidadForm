51496a6deea73c62acd50319d0d13dd1bfbf9cef
SebastianPegoraro <sebapego18@gmail.com>
2018-07-23 13:31:43 -0300

    First Commit

---
+++ foot.php
@@ -0,0 +1,20 @@
+<head>
+    <link rel="stylesheet" href="css/foot.css">
+</head>
+
+<?php
+include 'base.html'
+?>
+
+<footer class="footer" id="foot">
+    <div class="container">
+        <div class="row logos text-center">
+            <div class="col-12 col-md-6">
+                <img src="img/secretaria.png" alt="">
+            </div>
+            <div class="col-12 col-md-6">
+                <img src="img/direccion.png" alt="">
+            </div>
+        </div>
+    </div>
+</footer>
